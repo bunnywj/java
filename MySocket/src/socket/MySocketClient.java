1cb7794931212860ab8db854a9ebeb790366134b
bunnywj <bunny8301@gmail.com>
2014-06-09 22:38:38 +0800

    second

--- MySocket/src/socket/MySocketClient.java
+++ MySocket/src/socket/MySocketClient.java
@@ -5,42 +5,50 @@ import java.io.*;
 
 public class MySocketClient {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 
 		if ((args.length < 1) || (args.length > 2)) {
 			throw new IllegalArgumentException(
 					"Parameter(s): <Server> [<Port>]");
 		}
 
-		String server = args[0]; 
+		String server = args[0];
 		int serverPort = (args.length == 2) ? Integer.parseInt(args[1]) : 12345;
 
 		System.out.println("Connected to server...sending string. ");
-		System.out
-				.println("The client does not stop until input \" exit \".");
+		System.out.println("The client does not stop until input \"exit\".");
 
-		while (true) {
+		try {
+
 			Socket socket = new Socket(server, serverPort);
 
-			DataInputStream in = new DataInputStream(socket.getInputStream());
-			DataOutputStream out = new DataOutputStream(
+			DataInputStream inputFromServer = new DataInputStream(
+					socket.getInputStream());
+			DataOutputStream outputToServer = new DataOutputStream(
 					socket.getOutputStream());
+			
+			while (true) {
+				System.out.print("Please Enter: \t ");
+				String data = new BufferedReader(new InputStreamReader(
+						System.in)).readLine();
+				outputToServer.writeUTF(data);
+				outputToServer.flush();
+				System.out.println("Client sending: " + data);
 
-			System.out.print("Please Enter: \t ");
-			String data = new BufferedReader(new InputStreamReader(System.in))
-					.readLine();
-			out.writeUTF(data);
-			System.out.println("Client sending: " + data);
-			String rev = in.readUTF();
-			System.out.println("Server feedback: " + rev);
+				socket.setSoTimeout(5 * 1000);
 
-			if (rev.equals("exit")) {
-				System.out.println("The connect will be closed by client. ");
-				break;
+				String rev = inputFromServer.readUTF();
+				System.out.println("Server feedback: " + rev);
+
+				if (rev.equals("exit")) {
+					System.out
+							.println("The connect will be closed by client. ");
+					break;
+				}
+
 			}
-			in.close();
-			out.close();
-	//		socket.close();
+		} catch (IOException ex) {
+			System.err.println(ex);
 		}
 	}
 }
--- MySocket/src/socket/MySocketServer.java
+++ MySocket/src/socket/MySocketServer.java
@@ -5,34 +5,63 @@ import java.io.*;
 
 public class MySocketServer {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 
 		if (args.length != 1)
 			throw new IllegalArgumentException("Parameter(s): <Port>");
 
 		int serverPort = Integer.parseInt(args[0]);
-		//int serverPort = 12345;
-		ServerSocket serverSocket = new ServerSocket(serverPort);
-		
-		while (true) {
-			Socket socket = serverSocket.accept();
+		// int serverPort = 12345;
+		try {
+			int clientCount = 1;
 
-			SocketAddress clientAddress = socket.getRemoteSocketAddress();
-			System.out.println("Handling client at " + clientAddress);
+			ServerSocket serverSocket = new ServerSocket(serverPort);
 
-			DataInputStream in = new DataInputStream(socket.getInputStream());
-			String revClient = in.readUTF();
-			System.out.println("Received client: " + revClient);
+			while (true) {
+				Socket socket = serverSocket.accept();
+				InetAddress clientAddress = socket.getInetAddress();
+				System.out.println("client" + clientCount +"'s homename is " + clientAddress.getHostName());
+				System.out.println("client" + clientCount +"'s IP Address is " + clientAddress.getHostAddress());
+				
+				HandleAClient task = (new MySocketServer()).new HandleAClient(
+						socket);
+				new Thread(task).start();
 
-			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-			System.out.print("Respond client, Please Enter: \t ");
-			String sedServer = new BufferedReader(new InputStreamReader(
-					System.in)).readLine();
-			out.writeUTF(sedServer);
-			
-			in.close();
-			out.close();
-			//socket.close();
+				++clientCount;
+			}
+		} catch (IOException ex) {
+			System.err.println(ex);
+		}
+	}
+
+	public class HandleAClient implements Runnable {
+		private Socket socket;
+
+		public HandleAClient(Socket socket) {
+			this.socket = socket;
+		}
+
+		public void run() {
+			try {
+				DataInputStream inputFromClient = new DataInputStream(
+						socket.getInputStream());
+				DataOutputStream outputToClient = new DataOutputStream(
+						socket.getOutputStream());
+				while (true) {
+					String revClient = inputFromClient.readUTF();
+					System.out.println("Received client: " + revClient);
+
+					socket.setSoTimeout(5 * 1000);
+					
+					System.out.print("Respond client, Please Enter: \t ");
+					String sedServer = new BufferedReader(
+							new InputStreamReader(System.in)).readLine();
+					outputToClient.writeUTF(sedServer);
+					outputToClient.flush();
+				}
+			} catch (IOException ex) {
+				System.err.println(ex);
+			}
 		}
 	}
 }
