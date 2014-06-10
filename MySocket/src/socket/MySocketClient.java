6508642d0e44eb81ecf841a2e644cceb5f0f2286
bunnywj <bunny8301@gmail.com>
2014-06-10 23:10:27 +0800

    third

--- MySocket/src/socket/MySocketClient.java
+++ MySocket/src/socket/MySocketClient.java
@@ -7,36 +7,38 @@ public class MySocketClient {
 
 	public static void main(String[] args) {
 
+		// 控制台输入服务器IP和端口号，默认端口号为12345
 		if ((args.length < 1) || (args.length > 2)) {
 			throw new IllegalArgumentException(
 					"Parameter(s): <Server> [<Port>]");
 		}
-
+		// 服务器IP字符串
 		String server = args[0];
+		// 服务器端口号，可选项，默认12345
 		int serverPort = (args.length == 2) ? Integer.parseInt(args[1]) : 12345;
 
 		System.out.println("Connected to server...sending string. ");
 		System.out.println("The client does not stop until input \"exit\".");
 
-		try {
-
-			Socket socket = new Socket(server, serverPort);
-
-			DataInputStream inputFromServer = new DataInputStream(
-					socket.getInputStream());
-			DataOutputStream outputToServer = new DataOutputStream(
-					socket.getOutputStream());
-			
-			while (true) {
+		while (true) {
+			Socket socket = null;
+			try {
+				// 创建客户端Socket，输入服务器端口号和IP
+				socket = new Socket(server, serverPort);
+				//定义输入输出流
+				DataInputStream inputFromServer = new DataInputStream(
+						socket.getInputStream());
+				DataOutputStream outputToServer = new DataOutputStream(
+						socket.getOutputStream());
+				//给服务器发送消息
 				System.out.print("Please Enter: \t ");
 				String data = new BufferedReader(new InputStreamReader(
 						System.in)).readLine();
 				outputToServer.writeUTF(data);
-				outputToServer.flush();
-				System.out.println("Client sending: " + data);
+				//outputToServer.flush();
 
-				socket.setSoTimeout(5 * 1000);
-
+				// socket.setSoTimeout(5 * 1000);
+				//接收服务器返回的消息
 				String rev = inputFromServer.readUTF();
 				System.out.println("Server feedback: " + rev);
 
@@ -45,10 +47,13 @@ public class MySocketClient {
 							.println("The connect will be closed by client. ");
 					break;
 				}
-
-			}
-		} catch (IOException ex) {
-			System.err.println(ex);
+				//关闭输入输出流和socket
+				outputToServer.close();
+				inputFromServer.close();
+				socket.close();
+			} catch (IOException ex) {
+				System.err.println(ex);
+			} 
 		}
 	}
 }
--- MySocket/src/socket/MySocketServer.java
+++ MySocket/src/socket/MySocketServer.java
@@ -5,60 +5,75 @@ import java.io.*;
 
 public class MySocketServer {
 
+	static int serverPort = 0;
+
 	public static void main(String[] args) {
 
 		if (args.length != 1)
 			throw new IllegalArgumentException("Parameter(s): <Port>");
 
-		int serverPort = Integer.parseInt(args[0]);
-		// int serverPort = 12345;
-		try {
-			int clientCount = 1;
+		serverPort = Integer.parseInt(args[0]);
 
+		System.out.println("The server starts...");
+		MySocketServer server = new MySocketServer();
+		server.init();
+	}
+
+	public void init() {
+		try {
+//			int clientCount = 1;
+			//服务器端Socket
 			ServerSocket serverSocket = new ServerSocket(serverPort);
 
 			while (true) {
+				//监听客户端消息
 				Socket socket = serverSocket.accept();
-				InetAddress clientAddress = socket.getInetAddress();
-				System.out.println("client" + clientCount +"'s homename is " + clientAddress.getHostName());
-				System.out.println("client" + clientCount +"'s IP Address is " + clientAddress.getHostAddress());
-				
-				HandleAClient task = (new MySocketServer()).new HandleAClient(
-						socket);
-				new Thread(task).start();
+				//输出客户端地址，有问题，会导致阻塞
+//				InetAddress clientAddress = socket.getInetAddress();
+//				System.out.println("client" + clientCount + "'s homename is "
+//						+ clientAddress.getHostName());
+//				System.out.println("client" + clientCount + "'s IP Address is "
+//						+ clientAddress.getHostAddress());
 
-				++clientCount;
+				new HandleAClient(socket);
+
+//				++clientCount;
 			}
 		} catch (IOException ex) {
 			System.err.println(ex);
 		}
 	}
 
-	public class HandleAClient implements Runnable {
+	private class HandleAClient implements Runnable {
 		private Socket socket;
 
 		public HandleAClient(Socket socket) {
 			this.socket = socket;
+			new Thread(this).start();
 		}
 
 		public void run() {
 			try {
+				// 读取客户端数据
 				DataInputStream inputFromClient = new DataInputStream(
 						socket.getInputStream());
+				String revClient = inputFromClient.readUTF();
+				//查看客户端消息
+				System.out.println("Received client: " + revClient);
+
+				// socket.setSoTimeout(5 * 1000);
+				// 向客户端回复信息
 				DataOutputStream outputToClient = new DataOutputStream(
 						socket.getOutputStream());
-				while (true) {
-					String revClient = inputFromClient.readUTF();
-					System.out.println("Received client: " + revClient);
-
-					socket.setSoTimeout(5 * 1000);
-					
-					System.out.print("Respond client, Please Enter: \t ");
-					String sedServer = new BufferedReader(
-							new InputStreamReader(System.in)).readLine();
-					outputToClient.writeUTF(sedServer);
-					outputToClient.flush();
-				}
+				System.out.print("Respond client, Please Enter: \t ");
+				String sedServer = new BufferedReader(new InputStreamReader(
+						System.in)).readLine();
+				outputToClient.writeUTF(sedServer);
+				
+				//关闭输入输出流和socket
+				inputFromClient.close();
+				outputToClient.close();
+				socket.close();
 			} catch (IOException ex) {
 				System.err.println(ex);
 			}
